6dd0045501c8e622ee1c9dcd9fbeb61113eceef7
ionic5 <st.kaldalis@gmail.com>
2026-02-11 17:16:55 +0900

    2d 리소스 적용

--- LikeLion.LP0.Client/Assets/Scripts/UnityWorld/View/GameScene/Checkerboard.cs
+++ LikeLion.LP0.Client/Assets/Scripts/UnityWorld/View/GameScene/Checkerboard.cs
@@ -9,12 +9,14 @@ namespace LikeLion.LP0.Client.UnityWorld.View.GameScene
 {
     public class Checkerboard : MonoBehaviour, Core.View.GameScene.ICheckerboard
     {
-        [SerializeField]
-        private GameObject _blockPrefab;
+        //[SerializeField]
+        //private GameObject _blockPrefab;
         [SerializeField]
         private GameObject _stonePointPrefab;
         [SerializeField]
-        private float _gap;
+        private float _gapX;
+        [SerializeField]
+        private float _gapY;
         [SerializeField]
         private GameObject _root;
         [SerializeField]
@@ -48,68 +50,70 @@ namespace LikeLion.LP0.Client.UnityWorld.View.GameScene
             _activeStones = new List<Tuple<int, GameObject>>();
             _stonePoints = new System.Collections.Generic.List<System.Collections.Generic.List<StonePoint>>();
 
-            BuildCheckerboard(rows, cols);
+            //BuildCheckerboard(rows, cols);
             BuildStonePoints(rows, cols);
         }
 
-        private void BuildCheckerboard(int rows, int cols)
-        {
-            float boardWidth = cols * _blockSize + (cols - 1) * _gap;
-            float boardHeight = rows * _blockSize + (rows - 1) * _gap;
+        //private void BuildCheckerboard(int rows, int cols)
+        //{
+        //    float boardWidth = cols * _blockSize + (cols - 1) * _gap;
+        //    float boardHeight = rows * _blockSize + (rows - 1) * _gap;
 
-            void SpawnElement(Vector3 localPos, Vector3 localScale)
-            {
-                GameObject obj = Instantiate(_blockPrefab, _root.transform);
-                obj.transform.localPosition = localPos;
-                obj.transform.localScale = localScale;
-            }
+        //    void SpawnElement(Vector3 localPos, Vector3 localScale)
+        //    {
+        //        GameObject obj = Instantiate(_blockPrefab, _root.transform);
+        //        obj.transform.localPosition = localPos;
+        //        obj.transform.localScale = localScale;
+        //    }
 
-            for (int y = 0; y < rows; y++)
-            {
-                for (int x = 0; x < cols; x++)
-                {
-                    float localX = _offset.x + x * (_blockSize + _gap);
-                    float localZ = _offset.y + y * (_blockSize + _gap);
+        //    for (int y = 0; y < rows; y++)
+        //    {
+        //        for (int x = 0; x < cols; x++)
+        //        {
+        //            float localX = _offset.x + x * (_blockSize + _gap);
+        //            float localZ = _offset.y + y * (_blockSize + _gap);
 
-                    Vector3 localPos = new Vector3(localX, 0, localZ);
-                    SpawnElement(localPos, new Vector3(_blockSize, _blockSize, _blockSize));
-                }
-            }
+        //            Vector3 localPos = new Vector3(localX, 0, localZ);
+        //            SpawnElement(localPos, new Vector3(_blockSize, _blockSize, _blockSize));
+        //        }
+        //    }
 
-            float centerX = _offset.x + boardWidth / 2f - _blockPivot.x;
-            float centerZ = _offset.y + boardHeight / 2f - _blockPivot.z;
-            float edgeOffset = _gap + _blockSize / 2f;
+        //    float centerX = _offset.x + boardWidth / 2f - _blockPivot.x;
+        //    float centerZ = _offset.y + boardHeight / 2f - _blockPivot.z;
+        //    float edgeOffset = _gap + _blockSize / 2f;
 
-            Vector3 horizontalScale = new Vector3(boardWidth + _gap * 2 + _blockSize * 2, _blockSize, _blockSize);
-            Vector3 verticalScale = new Vector3(_blockSize, _blockSize, boardHeight + _gap * 2 + _blockSize * 2);
+        //    Vector3 horizontalScale = new Vector3(boardWidth + _gap * 2 + _blockSize * 2, _blockSize, _blockSize);
+        //    Vector3 verticalScale = new Vector3(_blockSize, _blockSize, boardHeight + _gap * 2 + _blockSize * 2);
 
-            // Top
-            SpawnElement(new Vector3(centerX, 0, _offset.y - edgeOffset - _blockPivot.z), horizontalScale);
-            // Bottom
-            SpawnElement(new Vector3(centerX, 0, _offset.y + boardHeight + edgeOffset - _blockPivot.z), horizontalScale);
-            // Left
-            SpawnElement(new Vector3(_offset.x - edgeOffset - _blockPivot.x, 0, centerZ), verticalScale);
-            // Right
-            SpawnElement(new Vector3(_offset.x + boardWidth + edgeOffset - _blockPivot.x, 0, centerZ), verticalScale);
-        }
+        //    // Top
+        //    SpawnElement(new Vector3(centerX, 0, _offset.y - edgeOffset - _blockPivot.z), horizontalScale);
+        //    // Bottom
+        //    SpawnElement(new Vector3(centerX, 0, _offset.y + boardHeight + edgeOffset - _blockPivot.z), horizontalScale);
+        //    // Left
+        //    SpawnElement(new Vector3(_offset.x - edgeOffset - _blockPivot.x, 0, centerZ), verticalScale);
+        //    // Right
+        //    SpawnElement(new Vector3(_offset.x + boardWidth + edgeOffset - _blockPivot.x, 0, centerZ), verticalScale);
+        //}
 
         private void BuildStonePoints(int rows, int cols)
         {
             int pointRows = rows + 1;
             int pointCols = cols + 1;
 
-            float startPointX = _offset.x - (_blockSize / 2f + _gap / 2f);
-            float startPointZ = _offset.y - (_blockSize / 2f + _gap / 2f);
+            float originPtX = _originPoint.transform.localPosition.x;
+            float originPtY = _originPoint.transform.localPosition.y;
+            float startPointX = originPtX;// + _offset.x - (_blockSize / 2f + _gap / 2f);
+            float startPointY = originPtY;// + _offset.y - (_blockSize / 2f + _gap / 2f);
 
             for (int y = 0; y < pointRows; y++)
             {
                 _stonePoints.Add(new System.Collections.Generic.List<StonePoint>());
                 for (int x = 0; x < pointCols; x++)
                 {
-                    float pX = startPointX + x * (_blockSize + _gap);
-                    float pZ = startPointZ + y * (_blockSize + _gap);
+                    float pX = startPointX + x * (_blockSize + _gapX);
+                    float pY = startPointY + y * (_blockSize + _gapY);
 
-                    Vector3 pointPos = new Vector3(pX, 0.1f, pZ);
+                    Vector3 pointPos = new Vector3(pX, pY, 0.0f);
 
                     GameObject pointObj = Instantiate(_stonePointPrefab, _root.transform);
                     pointObj.transform.localPosition = pointPos;
@@ -146,7 +150,7 @@ namespace LikeLion.LP0.Client.UnityWorld.View.GameScene
             _activeStones.Add(new Tuple<int, GameObject>(stoneType, stone));
 
             var pos = _stonePoints[row][column].transform.localPosition;
-            stone.transform.localPosition = new Vector3(pos.x, 0.6f, pos.z);
+            stone.transform.localPosition = new Vector3(pos.x, pos.y, 0.0f);
         }
 
         public void Clear()
